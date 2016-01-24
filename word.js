a6f2542a7823b834aee6705055213d8e5e9b274c
steph-harris <steph.harris21@gmail.com>
2016-01-24 16:34:47 -0500

    added property and functions to Word constructor, added most of the pseudocode for same

--- word.js
+++ word.js
@@ -3,5 +3,26 @@ var Letter = require("./letter.js");
 function Word(wrd){
   this.word = wrd;
   this.lets = [];
-  this.found = false
+  this.found = false;
+  this.getLets = function(){
+    //run loop w/ i =  length of word property after split{
+    //create new Letter object w/ current [i] passed in 
+    //push new object into lets [] }
+  }
+  this.checkIfLetterFound = function(guessLetter){
+    var whatToReturn = 0;
+    //run loop j over each object in lets[]
+    //compare charac[j] with guessLetter
+    //if(===){appear=true, checkIfLetterFound++, return checkIfLetterFound}
+  }
+  this.didWeFindTheWord = function(){
+
+  }
+  this.wordRender = function(){
+    var str = "";
+    //loop over let[]{call letterRender on [k], and .concat() on to str}
+    //return str
+  }
 }
+
+module.exports = Word;
